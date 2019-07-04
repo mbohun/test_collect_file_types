9dade18511556c7d0b21d3fe0163bdc32b828732
mbohun <martin.bohun@gmail.com>
2019-07-04 18:46:23 +1000

    a simple test helper script that will create N dir-s with some files in them, and populate the files with some random values; this will be a test input for some simple script-s i want to write and test

---
+++ make_dirs_with_files.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+FILE_TYPES=('bmp' 'c' 'doc' 'docx' 'jpg' 'jpeg' 'gif' 'md' 'odt' 'pdf' 'png' 'pnm' 'py' 'rtf' 'svg' 'tar' 'tiff' 'txt' 'xml' 'xz')
+FILE_TYPES_LEN=${#FILE_TYPES[*]}
+echo "FILE_TYPES_LEN: $FILE_TYPES_LEN"
+
+function setup_test_dir_struct {
+
+    # number of 'users' (user dir-s) to create
+    for n in `seq 0 10`
+    do
+        local user_dir_name="user-`printf "%.2d" ${n}`" 
+        mkdir -p user_dir_name
+
+        # create index.html file
+        rand_str=$RANDOM
+        echo "rand_str:${rand_str} => ${FILE_TYPES[$((rand_str % FILE_TYPES_LEN))]}"
+    done
+}
+
+#rm -rf ./user-*
