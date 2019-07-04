6c15eeaf3085c31043580955c4662047d3293554
mbohun <martin.bohun@gmail.com>
2019-07-05 00:00:03 +1000

    ADD: added creation of index.html per each user dir; the index.html refers/lists all the generated file-s in the user's dir as HTML <a href> links

--- make_dirs_with_files.sh
+++ make_dirs_with_files.sh
@@ -12,11 +12,29 @@ function setup_test_dir_struct {
         local user_dir_name="user-`printf "%.2d" ${n}`"
         mkdir -p ${user_dir_name}
 
+        local user_index_file=${user_dir_name}/index.html
+
+        # write the index.html header
+        echo "<html>"                                   > ${user_index_file}
+        echo "    <head>"                              >> ${user_index_file}
+        echo "        <title>${user_dir_name}</title>" >> ${user_index_file}
+        echo "    </head>"                             >> ${user_index_file}
+        echo "    <body>"                              >> ${user_index_file}
+        echo "        <h2>attachments</h2>"            >> ${user_index_file}
+
         for f in `seq 0 $(($RANDOM % 10))`
         do
             local test_file_name="test_file-`date +%s.%N`.${FILE_TYPES[$(($RANDOM % FILE_TYPES_LEN))]}"
             touch ${user_dir_name}/${test_file_name}
+
+            # add the $test_file_name to the index.html
+            echo "        <a href=\"${test_file_name}\">${test_file_name}</a>" >> ${user_index_file}
+            echo "        <p></p>" >> ${user_index_file}
         done
+
+        # write the index.html footer
+        echo "    </body>" >> ${user_index_file}
+        echo "</html>" >> ${user_index_file}
     done
 }
 
