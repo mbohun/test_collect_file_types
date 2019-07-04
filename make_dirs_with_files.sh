125425b8f4655fe6d42fc19353a12079c7458273
mbohun <martin.bohun@gmail.com>
2019-07-04 23:12:10 +1000

    INDENTATION: ONLY replaced tabs with spaces (emacs untabify)

--- make_dirs_with_files.sh
+++ make_dirs_with_files.sh
@@ -9,14 +9,14 @@ function setup_test_dir_struct {
     # number of 'users' (user dir-s) to create
     for n in `seq 0 10`
     do
-        local user_dir_name="user-`printf "%.2d" ${n}`" 
+        local user_dir_name="user-`printf "%.2d" ${n}`"
         mkdir -p ${user_dir_name}
 
-	for f in `seq 0 $(($RANDOM % 10))`
-	do
-	    local test_file_name="test_file-`date +%s.%N`.${FILE_TYPES[$(($RANDOM % FILE_TYPES_LEN))]}"
-	    touch ${user_dir_name}/${test_file_name}
-	done
+        for f in `seq 0 $(($RANDOM % 10))`
+        do
+            local test_file_name="test_file-`date +%s.%N`.${FILE_TYPES[$(($RANDOM % FILE_TYPES_LEN))]}"
+            touch ${user_dir_name}/${test_file_name}
+        done
     done
 }
 
