94d080a160f670eacd7028e725865192673ca5c9
mbohun <martin.bohun@gmail.com>
2019-07-05 13:13:27 +1000

    CLEANUP: some cleanup, added TODOs

--- extract_file_types_stats.sh
+++ extract_file_types_stats.sh
@@ -10,18 +10,23 @@ WORK_DIR=`mktemp -td ${script_name}.XXXXXX`
 ALL_FILES_EXT=${WORK_DIR}/all_files_ext.out
 ALL_FILES_EXT_STATS=${WORK_DIR}/all_files_ext_stats.out
 
+# TODO: This is OBVIOUSLY the simplest or rather NAIVE approach, we have to
+#       handle:
+#       - multiple '.' in dir and file name-s
+#       - whitespaces in dir and file name-s (so popular with windows users)
+#       - TODO
+#
 find ${1} -type f | sed -e 's/^.*\.//g' | sort > ${ALL_FILES_EXT}
 for ext in `cat ${ALL_FILES_EXT} | uniq`
 do
     echo "`grep -c $ext ${ALL_FILES_EXT}` $ext" >> ${ALL_FILES_EXT_STATS}
-    #printf "%${padding}.d %s\n" `grep -c ${ext} ${ALL_FILES_EXT}` ${ext} >> ${ALL_FILES_EXT_STATS}
 done
 
+# TODO: cleanup the var names
 cat ${ALL_FILES_EXT_STATS} | sort -nr > ${WORK_DIR}/sorted.out
 
 # NOTE: the padding is for the printf padding/formatting of the final result
 padding=$((`cat ${WORK_DIR}/sorted.out | head -n1 | sed -e 's/ .*$//g' | wc -m` - 1))
-#echo "padding: $padding"
 
 cat ${WORK_DIR}/sorted.out | xargs printf "%${padding}.d %s\n" > ${WORK_DIR}/result.out
 cat ${WORK_DIR}/result.out
