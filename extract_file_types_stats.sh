a902886dc7575c7ea2b78fa96dd145046c88f59f
mbohun <martin.bohun@gmail.com>
2019-07-05 12:57:59 +1000

    ADD: added simple script to extract the stats about file types (based on file ext): 11 html  8 bmp  7 c  6 doc  5 xz  5 tiff  5 md  4 txt  3 xml  3 tar  3 pnm  3 pdf  3 jpeg  2 svg  2 rtf  2 gif  1 png  1 odt  1 jpg  1 docx

---
+++ extract_file_types_stats.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+dir_to_search=${1}
+#echo "dir_to_search: ${dir_to_search}"
+
+script_name=`basename ${0}`
+WORK_DIR=`mktemp -td ${script_name}.XXXXXX`
+#echo "WORK_DIR: ${WORK_DIR}"
+
+ALL_FILES_EXT=${WORK_DIR}/all_files_ext.out
+ALL_FILES_EXT_STATS=${WORK_DIR}/all_files_ext_stats.out
+
+find ${1} -type f | sed -e 's/^.*\.//g' | sort > ${ALL_FILES_EXT}
+for ext in `cat ${ALL_FILES_EXT} | uniq`
+do
+    echo "`grep -c $ext ${ALL_FILES_EXT}` $ext" >> ${ALL_FILES_EXT_STATS}
+    #printf "%${padding}.d %s\n" `grep -c ${ext} ${ALL_FILES_EXT}` ${ext} >> ${ALL_FILES_EXT_STATS}
+done
+
+cat ${ALL_FILES_EXT_STATS} | sort -nr > ${WORK_DIR}/sorted.out
+
+# NOTE: the padding is for the printf padding/formatting of the final result
+padding=$((`cat ${WORK_DIR}/sorted.out | head -n1 | sed -e 's/ .*$//g' | wc -m` - 1))
+#echo "padding: $padding"
+
+cat ${WORK_DIR}/sorted.out | xargs printf "%${padding}.d %s\n" > ${WORK_DIR}/result.out
+cat ${WORK_DIR}/result.out
+
+#rm -rf ${WORK_DIR}
